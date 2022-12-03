7d4501fd4c2435a57181ba101a632dd8c7fc9d3f
Jannis Limperg <jannis@limperg.de>
2022-12-03 17:03:38 +0100

    Add license

--- Main.lean
+++ Main.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2022 Jannis Limperg. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jannis Limperg
+-/
+
 import Lean.Data.HashSet
 
 open IO
